01cbcf1bab00842833c87e327b78faabf84222c1
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-03-19 22:02:28 +0100

    ejercicios--strings

--- js/scripts.js
+++ js/scripts.js
@@ -59,3 +59,150 @@ const playAbby = (word1, word2) => {
 };
 playAbby('maca', 'effy');
 playAbby('maca', 'alfito');
+
+// 6 Camila quiere asegurarse de que un número de teléfono tiene el formato correcto. Debe tener exactamente 9 caracteres. Crea una función que reciba un número como string y determine si es válido o no.
+
+const numberPhone = number => {
+  if (number.length === 9) {
+    console.log('Es un numero de teléfono');
+  } else {
+    console.log('No es un numero de teléfono');
+  }
+};
+numberPhone('918756520');
+numberPhone('1544');
+
+// 7 Bego está eligiendo un dominio para su nueva web. Si la web termina en ".com", se usará para negocios. Si termina en ".org", será para una organización. Si no es ninguno de estos, el dominio no es válido. Crea una función que reciba un dominio y determine su clasificación.
+
+const checkDomain = mail => {
+  if (mail.endsWith('.com')) {
+    console.log('Es un dominio para negocios.');
+  } else if (mail.endsWith('.org')) {
+    console.log('Es un dominio para una organización.');
+  } else {
+    console.log('No es un dominio válido.');
+  }
+};
+checkDomain('macarena@mc.com');
+checkDomain('macarena@mc.org');
+checkDomain('macarena@mc.nm');
+
+// 8 Sabrina está revisando una lista de palabras. Si una palabra contiene la letra "z", la marcará como especial. Si no, será una palabra común. Crea una función que reciba una palabra y determine su categoría.
+
+const listWords = word => {
+  if (word.includes('z')) {
+    console.log('Es una palabra especial.');
+  } else {
+    console.log('Es una palabra común.');
+  }
+};
+listWords('zara');
+listWords('roma');
+
+// 9 Macarena está clasificando documentos según su urgencia. Si un mensaje contiene la palabra "urgente" en cualquier parte, lo priorizará. Si no, será un mensaje normal. Crea una función que reciba un mensaje y determine su nivel de prioridad.
+
+const urgentMessage = message => {
+  if (message.includes('urgente')) {
+    console.log('Es un mensaje urgente.');
+  } else {
+    console.log('Es un mensaje normal.');
+  }
+};
+urgentMessage('Vamos a poner aquí algo');
+urgentMessage('Este si es urgente ¡leeme plis!.');
+
+// 10 Abby encontró una nota con un código en una casa abandonada. Si el código es un número par y tiene exactamente 4 dígitos, puede ser la combinación de una caja fuerte. Si es impar o tiene otra longitud, lo descartará. Crea una función que reciba un código y determine si Abby debe investigarlo.
+
+const secretCode = code => {
+  if (code % 2 === 0 && code.length === 4) {
+    console.log('Investiga Abby, esto puede ser el código.');
+  } else {
+    console.log('Esto es un numero random.');
+  }
+};
+secretCode('2000');
+secretCode('0277');
+
+// 11 Camila está revisando invitaciones para un evento. Si el nombre del invitado empieza con "A", "E", "I", "O" o "U", se le asignará una mesa especial. Si empieza con otra letra, se le asignará una mesa normal. Crea una función que reciba un nombre y determine su ubicación.
+
+const checkInvitation = name => {
+  const vowels = 'AEIOU';
+  if (vowels.includes(name.charAt(0))) {
+    console.log('¡Enhorabuena! estás sentado en una mesa especial.');
+  } else {
+    console.log('Vas a una mesa normalita.');
+  }
+};
+checkInvitation('Macarena');
+checkInvitation('Antoniaaa');
+
+// 12 Bego está verificando si un texto contiene lenguaje ofensivo. Si un mensaje contiene la palabra "tonto" o "feo", se marcará como inapropiado. Si no, será aceptado. Crea una función que reciba un mensaje y determine su categoría.
+
+const checkLanguage = message => {
+  if (message.includes('tonto') || message.includes('feo')) {
+    console.log('Es un mensaje inapropiado.');
+  } else {
+    console.log('Mensaje aceptado.');
+  }
+};
+checkLanguage('Eres más feo que pegar a un padre.');
+checkLanguage('Hola developers preciosos.');
+
+// 13 Sabrina quiere imprimir etiquetas con iniciales. Crea una función que reciba un nombre y un apellido y devuelva sus iniciales en mayúsculas, separadas por un punto.
+
+const printLabels = (names, surname) => {
+  const firstLetterName = names.charAt(0).toUpperCase();
+  const firstLetterSurname = surname.charAt(0).toUpperCase();
+  console.log(firstLetterName + '.' + firstLetterSurname);
+};
+printLabels('macarena', 'carrero');
+
+// 14 Macarena está diseñando una campaña publicitaria. Si el título del anuncio tiene más de 20 caracteres, lo reducirá. Si tiene 20 o menos, lo usará tal cual. Crea una función que reciba un título y determine si debe ajustarse.
+
+const advertisingTitle = title => {
+  if (title.length > 20) {
+    console.log('Hay que reducir el título.');
+  } else {
+    console.log(title);
+  }
+};
+advertisingTitle('Hola soy Macarena y me quedan muchos ejercicios.');
+advertisingTitle('Este vale?'); // como no hay que reducir te devuelve el mensaje
+
+// 15 Abby encontró una radio funcionando con interferencias. Si la señal incluye la palabra "ayuda" o "suministros", significa que alguien está pidiendo asistencia. Si no contiene estas palabras, lo ignorará. Crea una función que reciba un mensaje de radio y determine si Abby debe responder.
+
+const helpMessage = message => {
+  if (message.includes('ayuda') || message.includes('suministros')) {
+    console.log('Están pidiendo ayuda.');
+  } else {
+    console.log('Pasa de este mensaje.');
+  }
+};
+helpMessage('No necesito nada.');
+helpMessage('Estoy cansada de hacer esto, necesito ayuda.');
+helpMessage('Necesito suministros.');
+
+// 16 Camila quiere asegurarse de que los nombres de usuario no contengan espacios. Si un nombre tiene espacios, será inválido. Si no tiene, será aceptado. Crea una función que reciba un nombre de usuario y determine su validez.
+
+const noSpacesUserName = userName => {
+  if (userName.includes(' ')) {
+    console.log('Nombre de usuario inválido.');
+  } else {
+    console.log('Nombre de usuario aceptado.');
+  }
+};
+noSpacesUserName('maquitauser');
+noSpacesUserName('user macarena');
+
+// 17 Bego está registrando nuevas cuentas de clientes. Si un correo electrónico contiene "@" y ".", será válido. Si no tiene alguno de estos elementos, será inválido. Crea una función que reciba un email y determine si es correcto.
+
+const newCounts = email => {
+  if (email.includes('@') && email.includes('.')) {
+    console.log('Correo electrónico válido.');
+  } else {
+    console.log('Es un correo inválido.');
+  }
+};
+
+newCounts('macarena@gmail.com');
+newCounts('macarena@');
